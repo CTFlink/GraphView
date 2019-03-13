1a3d75c0a78cdc34f70d7ba71880d83fef023559
András Ács <5522871+andracs@users.noreply.github.com>
2019-03-13 12:56:20 +0100

    Flytter kodeeksemplet ind i onCreate(), og tilføjer graph.addSeries(series);

--- app/src/main/java/com/ctflink/graphview/MainActivity.java
+++ app/src/main/java/com/ctflink/graphview/MainActivity.java
@@ -13,16 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
     }
-
-    GraphView graph = (GraphView) findViewById(R.id.graph);
-    LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-            new DataPoint(0, 1),
-            new DataPoint(1, 5),
-            new DataPoint(2, 3),
-            new DataPoint(3, 2),
-            new DataPoint(4, 6)
-    });
-
+    
 
 }
